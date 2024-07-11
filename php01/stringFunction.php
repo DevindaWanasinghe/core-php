8d5188c9173d01e0c473941d3aa73e7fe9f8fe9d
devindawanasinghe <devindawanasinghe@gmail.com>
2024-07-11 18:24:07 +0530

    php-variable/ strings/ string functions

--- php01/stringFunction.php
+++ php01/stringFunction.php
@@ -1,5 +1,5 @@
 <?php 
-    $first = "The first boxing wizard ";
+    $first = "the first boxing wizard ";
     $second = "jump quickly.";
 
     //$sentence = $first . $second;
@@ -11,5 +11,20 @@
 ?>
 <br>
 All simple: <?php echo strtolower($sentence); //strtolower function ?> <br>  
+
 All capital: <?php echo strtoupper($sentence);  //strtoupper function?> <br>
+
 First letter capital: <?php echo ucfirst($sentence);  //ucfirst function?> <br>
+
+First letter capital in all words: <?php echo ucwords($sentence);  //ucword function ?> <br>
+
+Number of characters: <?php echo strlen($sentence);  //number of characters ?> <br>
+
+Search for a string or character: <?php echo strstr($sentence, "box");  //number of characters ?> <br>
+
+replace words: <?php echo str_replace("first","seven",$sentence);  //replace "first" word for "seven" ?> <br>
+
+
+
+
+
